9df8ecf586bb68af3b7c9fbac42126c5aca3045e
Vishnuteja <vishnuteja476@gamil.com>
2022-05-28 14:42:22 +0530

    Notes class created

---
+++ app/src/main/java/com/example/mytasks/Notes.java
@@ -0,0 +1,28 @@
+package com.example.mytasks;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Notes_Database")
+public class Notes {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "notes_title")
+    String notesTitle;
+
+    @ColumnInfo(name = "note_subtitle")
+    String notesSubtitle;
+
+    @ColumnInfo(name = "notes_date")
+    String date;
+
+    @ColumnInfo(name = "notes")
+    String notes;
+
+    @ColumnInfo(name = "notes_priority")
+    String notesPriority;
+
+}
