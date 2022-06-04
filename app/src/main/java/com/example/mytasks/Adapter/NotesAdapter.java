d0f6edcc2061bdd4d95c810518a2c9f2b8826aa7
Vishnuteja <vishnuteja476@gamil.com>
2022-06-04 15:40:45 +0530

    added UI for note item and displays list of itmes on screen

---
+++ app/src/main/java/com/example/mytasks/Adapter/NotesAdapter.java
@@ -0,0 +1,75 @@
+package com.example.mytasks.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mytasks.MainActivity;
+import com.example.mytasks.Model.Notes;
+import com.example.mytasks.R;
+
+import java.util.List;
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.notesViewHolder> {
+
+    MainActivity mainActivity ;
+    List<Notes> notes;
+
+    public NotesAdapter(MainActivity mainActivity, List<Notes> notes) {
+        this.mainActivity = mainActivity;
+        this.notes = notes;
+    }
+
+    @NonNull
+
+    @Override
+    public notesViewHolder onCreateViewHolder(@NonNull  ViewGroup parent, int viewType) {
+
+        return new notesViewHolder( LayoutInflater.from(mainActivity ).inflate( R.layout.item_notes,parent,false ) );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull  NotesAdapter.notesViewHolder holder, int position) {
+
+        Notes note = notes.get(position);
+
+        if(note.notesPriority.equals( "1" )){
+            holder.notesPriority.setBackgroundResource( R.drawable.green_shape );
+        }else if(note.notesPriority.equals("2")){
+            holder.notesPriority.setBackgroundResource( R.drawable.yellow_shape );
+        }else if(note.notesPriority.equals("3")){
+            holder.notesPriority.setBackgroundResource( R.drawable.red_shape );
+        }
+
+
+        holder.title.setText( note.notesTitle );
+        holder.subtitle.setText( note.notesSubtitle );
+        holder.notesDate.setText( note.date ); // might be error
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+   static class notesViewHolder extends RecyclerView.ViewHolder{
+
+        TextView title,subtitle,notesDate;
+        View notesPriority;
+
+        public notesViewHolder(@NonNull  View itemView) {
+            super( itemView );
+
+            title = itemView.findViewById( R.id.notesTitle );
+            subtitle = itemView.findViewById( R.id.notesSubtitle );
+            notesDate = itemView.findViewById( R.id.notesDate );
+            notesPriority = itemView.findViewById( R.id.notesPriority );
+        }
+    }
+
+}
--- app/src/main/java/com/example/mytasks/MainActivity.java
+++ app/src/main/java/com/example/mytasks/MainActivity.java
@@ -1,26 +1,43 @@
 package com.example.mytasks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.mytasks.Activity.InsertNote;
+import com.example.mytasks.Adapter.NotesAdapter;
+import com.example.mytasks.ViewModel.NotesViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton newNotesbtn;
+    NotesViewModel notesViewModel;
+    RecyclerView notesRecycler;
+    NotesAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
 
         newNotesbtn = findViewById( R.id.newNotesbtn );
+        notesRecycler = findViewById( R.id.notesRecycler );
+
+        notesViewModel = new ViewModelProvider(this).get(NotesViewModel.class);
 
         newNotesbtn.setOnClickListener( v -> {
             startActivity( new Intent(MainActivity.this, InsertNote.class) );
+        } );
+
+        notesViewModel.getAllNotes.observe( this,notes -> {
+               notesRecycler.setLayoutManager( new LinearLayoutManager( this ) );
+               adapter = new NotesAdapter(MainActivity.this,notes);
+               notesRecycler.setAdapter( adapter );
         } );
 
     }
